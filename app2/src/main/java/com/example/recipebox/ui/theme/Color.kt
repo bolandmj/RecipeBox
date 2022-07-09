54ded664a5d6c4d61cb68681b6fd949a29acea69
finchea <66384814+finchea@users.noreply.github.com>
2022-07-09 15:28:39 -0400

    Commit

--- app2/src/main/java/com/example/recipebox/ui/theme/Color.kt
+++ app2/src/main/java/com/example/recipebox/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.recipebox.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Red200 = Color(0xFFFF544F)
+val Red200 = Color(0xFFFF544E)
 val Red500 = Color(0xFFcc453b)
 val Red700 = Color(0xFF822c26)
 val Blue200 = Color(0xFFb3e2ea)
