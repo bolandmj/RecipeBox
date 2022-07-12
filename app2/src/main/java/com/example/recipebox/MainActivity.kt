2b30cec0eb73f0a4e87b5001b6f8a0111309bd82
hegartim <hegartim@mail.uc.edu>
2022-07-12 13:18:45 -0400

    Added Search Bar

--- app2/src/main/java/com/example/recipebox/MainActivity.kt
+++ app2/src/main/java/com/example/recipebox/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.recipebox
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.recipebox.Scaffold
 import com.example.recipebox.ui.theme.RecipeBoxTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,6 +37,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Scaffold() {
+    var recipeName by remember { mutableStateOf("")}
+    val context = LocalContext.current
     val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
     Scaffold(
         scaffoldState = scaffoldState,
@@ -40,7 +46,20 @@ fun Scaffold() {
         content = { padding ->
             Column(modifier = Modifier
                 .padding(padding)) {
-                Text("Welcome to Recipe Box! [ADD CONTENT HERE IN SCAFFOLD]") }
+                Text("Welcome to Recipe Box! [ADD CONTENT HERE IN SCAFFOLD]")
+                OutlinedTextField(
+                    value = recipeName,
+                    onValueChange = { recipeName = it },
+                    label = { Text(stringResource(R.string.recipeName)) }
+                )
+                Button(
+                    onClick = {
+                        Toast.makeText(context, "$recipeName", Toast.LENGTH_LONG).show()
+                    }
+                ) {
+                    Text(text = "Search")
+                }
+            }
         },
     )
 }
