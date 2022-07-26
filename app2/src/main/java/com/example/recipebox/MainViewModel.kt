779ae9a5c2004b2a727dbf02f8480816d64981a9
hegartim <hegartim@mail.uc.edu>
2022-07-25 22:43:07 -0400

    Added MainViewModel

---
+++ app2/src/main/java/com/example/recipebox/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.recipebox
+
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainViewModel : ViewModel() {
+
+    private lateinit var firestore : FirebaseFirestore
+}
