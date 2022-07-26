431d63dfe709e2af8cca3c9715c99cb33855e0a2
hegartim <hegartim@mail.uc.edu>
2022-07-25 23:05:49 -0400

    Added Save function for when post method is finished

--- app2/src/main/java/com/example/recipebox/MainViewModel.kt
+++ app2/src/main/java/com/example/recipebox/MainViewModel.kt
@@ -2,8 +2,24 @@ package com.example.recipebox
 
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 class MainViewModel : ViewModel() {
 
     private lateinit var firestore : FirebaseFirestore
+
+    init{
+        firestore = FirebaseFirestore.getInstance()
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+    }
+
+    //Pass in recipe object to function when that is created (ex: save(specimen: Specimen)
+    fun save(){
+        val document = firestore.collection("recipes").document()
+        //example for when post function is made:
+        //specimen.specimenId = document.id
+        //val handle = document.set(specimen)
+        //handle.addOnSuccessListener { Log.d("Firebase", "Document Saved" }
+        //handle.addOnFailureListener { Log.e("Firebase", "Save failed $it " }
+    }
 }
